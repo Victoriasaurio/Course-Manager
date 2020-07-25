3e5f8fe7df48b40293c9037e8a2246d72e48e36a
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2020-07-25 17:22:22 -0500

    without changes

--- config/config.js
+++ config/config.js
@@ -1,8 +1,7 @@
-// Configuración del puerto y de la base de datos
 const config = {
     port: 3800,
     connexion: 'mongodb://localhost:27017/gestorcursos_db',
     host: 'http://localhost:3800/'
 }
 
-module.exports = config; // Se exporta la variable config para poder ser usada por otros archivos
+module.exports = config;
